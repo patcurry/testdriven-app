2eec7166f7775039aeb0d95e6e8e9692f43a01e8
Pat <currypat1985@gmail.com>
2019-07-29 10:47:37 +0200

    first commit

---
+++ services/users/project/config.py
@@ -0,0 +1,20 @@
+# services/users/project/config.py
+
+class BaseConfig:
+    """Base configuration"""
+    TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development Configuration"""
+    pass
+
+
+class TestingConfig(BaseConfig):
+    """Testing Configuration"""
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    """Production Configuration"""
+    pass
